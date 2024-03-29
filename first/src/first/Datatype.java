b7cc7c441fb96e54aeac5f590b5c0c4ba5ed8a4c
Dell <Dell@192.168.109.1>
2024-03-29 14:39:14 +0530

    First commit

---
+++ first/src/first/Datatype.java
@@ -0,0 +1,26 @@
+package first;
+
+public class Datatype {
+	
+	public static void main(String [] args) {
+		System.out.println("byte size:"+Byte.SIZE+ " bits");
+		System.out.println("short size:"+Short.SIZE+ " bits");
+		System.out.println("Int size:"+Integer.SIZE+ " bits");
+		System.out.println("long size:"+Long.SIZE+ " bits");
+		System.out.println("float size:"+Float.SIZE+ " bits");
+		System.out.println("double size:"+Double.SIZE+ " bits");
+		System.out.println("char size:"+Character.SIZE+ " bits");
+		
+		System.out.println("________________________");
+		System.out.println("Printing range for datatypes");
+		System.out.println("byte range:"+Byte.MIN_VALUE+" ->" +Byte.MAX_VALUE);
+		System.out.println("short range:"+Short.MIN_VALUE+" ->" +Short.MAX_VALUE);
+		System.out.println("Int range:"+Integer.MIN_VALUE+" ->" +Integer.MAX_VALUE);
+		System.out.println("long range:"+Long.MIN_VALUE+" ->" +Long.MAX_VALUE);
+		System.out.println("float range:"+Float.MIN_VALUE+" ->" +Float.MAX_VALUE);
+		System.out.println("double range:"+Double.MIN_VALUE+" ->" +Double.MAX_VALUE);
+		
+		
+	}
+
+}
---
+++ first/src/first/class1.java
@@ -0,0 +1,20 @@
+package first;
+
+class car{
+	int numberOfWheels= 4; //datatype variablename= propertyvalue;
+	String color = "red"; // datatype variable= propertyvalue; 
+}
+public class class1 {
+
+	public static void main(String[] args) {
+		//classNmae objName = new ClassName();
+		
+		car santro = new car();
+		System.out.println(santro);
+		System.out.println(santro.numberOfWheels);
+		System.out.println(santro.color);
+		
+
+	}
+
+}
